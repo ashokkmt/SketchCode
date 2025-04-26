2a1572177acdf33b5af93205babb49e1d2736e95
HimanshuBisht23L <himanshiubisht4532@gmail.com>
2025-04-26 17:27:45 +0530

    editor solve

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -8,12 +8,12 @@ import {
   SquareIcon,
   HexagonIcon,
   ArrowDownDoubleIcon,
-  ArrowUpDoubleIcon
+  ArrowUpDoubleIcon,
+  Menu01Icon
 } from "hugeicons-react";
 
 const tools = [
   { id: "hand", icon: <Hold03Icon color="#000" />, hold: true },
-  // { id: "edge", label: "Edge", icon: <LinerIcon color="#000" />, edge: true },
   { id: "pointer", icon: <Cursor02Icon color="#000" />, pointer: true },
   { id: "circle", icon: <OvalIcon color="#000" />, shape: true },
   { id: "rectangle", icon: <SquareIcon color="#000" />, shape: true },
@@ -44,14 +44,18 @@ export default function MinimalToolbar({ setSelectedShape, setShape, sendFlowBac
 
 
   const editorCode = () => {
-    setShowEditor(!showEditor)
+    if(showEditor){
+      setShowEditor(false)
+      return
+    }
+    setShowEditor(true)
     sendFlowBackend();
   }
 
 
   return (
     <div className="flex justify-between items-center mt-4 z-10">
-      <button className="hover:transform hover:scale-[1.05] active:scale-[0.98] transition-all duration-300 ease-in-out cursor-pointer self-start bg-amber-300 p-[.6rem] rounded-2xl ml-[1rem]" >HAMBurger</button>
+      <button className="hover:transform hover:scale-[1.05] active:scale-[0.98] transition-all duration-300 ease-in-out cursor-pointer self-start bg-gray-100 hover:bg-gray-200 p-[.6rem] rounded-2xl ml-[1rem]" > <Menu01Icon color="#000" /> </button>
       <div className="flex items-center gap-3 p-2 bg-white rounded-xl shadow border border-gray-200 w-fit mx-auto ">
         {tools.map((tool) => (
           <button
@@ -67,7 +71,7 @@ export default function MinimalToolbar({ setSelectedShape, setShape, sendFlowBac
           </button>
         ))}
       </div>
-      <button className="rotate-[90deg] z-999 hover:transform hover:scale-[1.05] hover:right-[-0%] active:scale-[0.98] transition-all duration-300 ease-in-out cursor-pointer self-start bg-amber-300 p-[.6rem] rounded-2xl absolute top-[45%] right-[-1%] pt-[1rem]" onClick={() => editorCode()}>{!showEditor ? <ArrowDownDoubleIcon color="#000" /> : <ArrowUpDoubleIcon color="#000" />}</button>
+      <button className="rotate-[90deg] z-999 hover:transform hover:scale-[1.05] hover:right-[-0%] active:scale-[0.98] transition-all duration-300 ease-in-out cursor-pointer self-start bg-gray-100 hover:bg-gray-200 p-[.6rem] rounded-2xl absolute top-[45%] right-[-1%] pt-[1rem]" onClick={() => editorCode()}>{!showEditor ? <ArrowDownDoubleIcon color="#000" /> : <ArrowUpDoubleIcon color="#000" />}</button>
 
     </div>
   );
